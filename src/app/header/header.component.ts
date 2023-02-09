e46c98799237b4f47e704b1b3884bf01bdf42d83
yanisa.p <yanisa.p@gofive.co.th>
2023-02-09 11:39:33 +0700

    update dropdown user header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { UserType } from '../enum/user-type.enum';
 import { User } from '../models/user.model';
 import { UserService } from '../services/user.service';
@@ -11,6 +12,11 @@ import { UserService } from '../services/user.service';
 export class HeaderComponent implements OnInit {
   public toggleDropdown:boolean = false
   public user:User
+  public userTypeList:string[] = [
+    UserType[0],
+    UserType[1]
+  ]
+  private userChanged$ : Subscription;
   get UserType(){
     return UserType
   }
@@ -19,10 +25,30 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = this.userService.getUser()
+    this.userChanged$ = this.userService.userChanged.subscribe(
+      (user:User) => {
+        this.user = user
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this.userChanged$?.unsubscribe()
   }
 
   onToggleDropdown(event: Event){
-    console.log('event', event)
+    // console.log('event', event)
     this.toggleDropdown = !this.toggleDropdown
+  }
+  onSelect(type:string){
+    switch (type) {
+      case UserType[0]:
+        this.user.userType = UserType.admin
+        break;
+      case UserType[1]:
+        this.user.userType = UserType.basic
+        break;
+    }
+    this.userService.changeUser(this.user)
   }
 }
